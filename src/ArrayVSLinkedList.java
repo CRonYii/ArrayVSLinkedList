0674c7bf2fd702ff7f13f9da1b85fda582eb3a64
CRonYii <kenchenrong@gmail.com>
2017-11-30 23:35:21 -0800

    Organize code perfectly

--- src/ArrayVSLinkedList.java
+++ src/ArrayVSLinkedList.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -10,82 +11,56 @@ import java.util.Random;
  */
 public class ArrayVSLinkedList {
     public static final int ITERATION = 100_000;
-    
+
     public static ArrayList<Integer> arrayList = new ArrayList<>();
     public static LinkedList<Integer> linkedList = new LinkedList<>();
-    
+
     public static Random rnd;
     public static int seed = 1;
-    
+
     public static long startTime;
     public static double timeTaken;
-    
+
     /**
+     * Drives the program
+     * 
      * @param args
+     *            unused
      */
     public static void main(String[] args) {
-        addElementsToArrayList();
-        addElementsToLinkedList();
-        randomAccessedToArrayList();
-        randomAccessedToLinkedList();
+        addElementsToList(arrayList); // adds 100,000 element to the arrayList
+        addElementsToList(linkedList); // adds 100,000 element to the linkedList
+        randomAccessedToList(arrayList); // access the arrayList at random position for 100,000 times
+        randomAccessedToList(linkedList); // access the linkedList at random position for 100,000 times
     }
-    
+
     /**
-     * Adds 100,000 elements to the ArrayList.<br>
+     * Adds 100,000 elements to the given List.<br>
      * Prints out the time taken.
      */
-    public static void addElementsToArrayList() {
+    public static void addElementsToList(List<Integer> list) {
         startTime = System.currentTimeMillis();
         for (int i = 0; i < ITERATION; i++) {
-            arrayList.add(0, i); // This is the line we care about.
+            list.add(0, i); // This is the line we care about.
         }
         timeTaken = (double) (System.currentTimeMillis() - startTime) / 1000.0;
-        System.out.println(String.format("Adding %s elements to an ArrayList took\n%s seconds", ITERATION, timeTaken));
+        System.out.println(String.format("Inserting %s elements took\n%s seconds", ITERATION, timeTaken));
     }
-    
+
     /**
-     * Adds 100,000 elements to the LinkedList.<br>
+     * Access the given List at random position for 100,000 time.<br>
      * Prints out the time taken.
      */
-    public static void addElementsToLinkedList() {
-        startTime = System.currentTimeMillis();
-        for (int i = 0; i < ITERATION; i++) {
-            linkedList.add(0, i); // This is the line we care about.
-        }
-        timeTaken = (double) (System.currentTimeMillis() - startTime) / 1000.0;
-        System.out.println(String.format("Adding %s elements to a LinkedList took\n%s seconds", ITERATION, timeTaken));
-        
-        System.out.println();
-    }
-    
-    /**
-     * Access the ArrayList at random position for 100,000 time.<br>
-     * Prints out the time taken.
-     */
-    public static void randomAccessedToArrayList() {
+    public static void randomAccessedToList(List<Integer> list) {
         rnd = new Random(seed);
         startTime = System.currentTimeMillis();
         for (int i = 0; i < ITERATION; i++) {
-            int index = rnd.nextInt(ITERATION);
-            arrayList.get(index); // This is the line we care about.
+            int index = rnd.nextInt(ITERATION); // randomly generated
+            list.get(index); // This is the line we care about.
         }
         timeTaken = (double) (System.currentTimeMillis() - startTime) / 1000.0;
-        System.out.println(String.format("Random accessed to an ArrayList for %s times took\n%s seconds", ITERATION, timeTaken));
-    }
-    
-    /**
-     * Access the LinkedList at random position for 100,000 time.<br>
-     * Prints out the time taken.
-     */
-    public static void randomAccessedToLinkedList() {
-        rnd = new Random(seed);
-        startTime = System.currentTimeMillis();
-        for (int i = 0; i < ITERATION; i++) {
-            int index = rnd.nextInt(ITERATION);
-            linkedList.get(index); // This is the line we care about.
-        }
-        timeTaken = (double) (System.currentTimeMillis() - startTime) / 1000.0;
-        System.out.println(String.format("Random accessed to a LinkedList for %s times took\n%s seconds", ITERATION, timeTaken));
+        System.out.println(
+                String.format("Random accessed for %s times took\n%s seconds", ITERATION, timeTaken));
     }
 
 }
